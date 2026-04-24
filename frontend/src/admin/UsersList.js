3b80eedf71299bfa4a18a22ea48ebcc05897b162
Rajapaksha W.T.D. <thisandirajapaksha@gmail.com>
2026-04-24 16:53:43 +0530

    Role Selection
    Adding Admin search bar and fixing css styles

--- frontend/src/admin/UsersList.js
+++ frontend/src/admin/UsersList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../styles/Admin.css'; // Ensure this path is correct
 
 const UsersList = () => {
     const [users, setUsers] = useState([]);
