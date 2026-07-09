14c56a534c75fc2dd9d8dbc2adae4705bd21340a
Naindu Janith <161099705+navijani@users.noreply.github.com>
2026-07-09 09:09:55 +0530

    logging error fix

--- frontend/src/components/LoginCredentials.js
+++ frontend/src/components/LoginCredentials.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Loader from './Loader';
 import '../styles/RoleSelection.css';
 
 const LoginCredentials = ({ role, onBack, onSuccess, onContactClick }) => { // Added onContactClick prop
